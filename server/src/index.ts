eb6197d1ae71348c4ec0b1307e8a2d057be96a7d
junsoo <jyhckb@naver.com>
2021-09-21 19:01:21 +0900

    Install dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import Router from 'koa-router'
@@ -6,10 +8,10 @@ import apiRouter from './api'
 
 const app = new Koa()
 const port: number = 3000
-const router = new Router()
 
 app.use(bodyParser())
 
+const router = new Router()
 router.use('/api', apiRouter.routes())
 app.use(router.routes())
 
